b13d98d905ff922cf5958a08dda973a8bd050210
Prashant-Kumar001 <prashant0kumar101@gmail.com>
2025-09-08 18:57:36 +0530

    new updated async fun

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-handler = (event, context, callback) => {
+exports.handler = async (event, context) => {
+    try {
+        const success = true; 
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    const err = {
-        statusCode: 500,
-        body: JSON.stringify('Error from Lambda!'),
+        if (success) {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify('Hello from Lambda!'),
+            };
+            return response; 
+        } else {
+            throw new Error('Something went wrong'); 
+        }
+    } catch (err) {
+        const errorResponse = {
+            statusCode: 500,
+            body: JSON.stringify({ message: err.message }),
+        };
+        return errorResponse; 
     }
-    callback(err, null);
 };
 
-const callback = (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+if (require.main === module) {
+    (async () => {
+        const event = {};
+        const context = {};
+
+        const result = await exports.handler(event, context);
+        console.log('Lambda Output:', result);
+    })();
 }
-
-const event = {};
-const context = {};
-
-handler( event, context, callback )
